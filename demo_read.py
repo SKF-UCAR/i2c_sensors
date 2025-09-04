e70f26514d7543ba3966420e8415826156468348
Slava Fedorchuk <slavafedor@ucar.edu>
2025-09-04 16:26:30 -0600

    improving demo, fixing issues

--- demo_read.py
+++ demo_read.py
@@ -1,3 +1,4 @@
+import argparse,time
 from i2c_sensors.base import I2CConfig
 from i2c_sensors.ina260 import INA260
 from i2c_sensors.adc128d818 import ADC128D818, ADC128D818Config
@@ -6,17 +7,37 @@ from i2c_sensors.adc128d818 import ADC128D818, ADC128D818Config
 INA_ADDR = 0x40  # A0/A1 strapping selects one of 16 addresses per datasheet
 ADC_ADDR = 0x1D  # A0/A1 tri-level allow 9 addresses
 
-# ina = INA260(I2CConfig(1, INA_ADDR))
-# ina.configure(avg=0, vbus_ct=0, ishunt_ct=0, mode=0b111)
-# print("INA260:", ina.to_dict())
-# ina.close()
+def main():
+    
+    ap = argparse.ArgumentParser(description="Simple I2C sensor reader")
+    ap.add_argument("--cont", type=bool, help="Continuous mode for ADC128D818")
+    ap.add_argument("--mask", type=int, default=0, help="Disable mask for ADC128D818 (bit i disables channel i)")
+    args = ap.parse_args()
 
-adc = ADC128D818(I2CConfig(1, ADC_ADDR))
-conf = ADC128D818Config( 
-            start = True,
-            continuous = True,
-            disable_mask = 0x00,
-            extResistorMultipliers = [2.7, 2.7, 2.7, 5.0, 5.0, 5.0, 2.0, 2.0] )
-adc.configure(conf)
-print("ADC128D818:", adc.read_channels())
-adc.close()
+    print("Args:", args)
+
+    # ina = INA260(I2CConfig(1, INA_ADDR))
+    # ina.configure(avg=0, vbus_ct=0, ishunt_ct=0, mode=0b111)
+    # print("INA260:", ina.to_dict())
+    # ina.close()
+
+    adc = ADC128D818(I2CConfig(1, ADC_ADDR))
+    conf = ADC128D818Config( 
+                start = True,
+                continuous = False if args.cont is None else args.cont,
+                disable_mask = args.mask & 0x0F,
+                mode = 1,
+                extResistorMultipliers = [2.7, 2.7, 2.7, 5.0, 5.0, 5.0, 2.0, 2.0] )
+    adc.configure(conf)
+    time.sleep(0.05)
+    print("ADC128D818:", adc.read_channels())
+    conf.mode = 0
+    conf.extResistorMultipliers = [2.7, 2.7, 2.7, 5.0, 5.0, 5.0, 2.0, 1.0]
+    adc.configure(conf)
+    time.sleep(0.05)
+    print("ADC128D818: Temp mode:", adc.read_channel(7))
+    adc.close()
+
+
+if __name__ == "__main__":
+    main()
--- i2c_sensors/adc128d818.py
+++ i2c_sensors/adc128d818.py
@@ -11,6 +11,7 @@ REG_CONV_RATE       = 0x07
 REG_CH_DISABLE      = 0x08
 REG_ONE_SHOT        = 0x09
 REG_DEEP_SHUTDOWN   = 0x0A
+REG_ADV_CONFIG      = 0x0B
 REG_READING_BASE    = 0x20  # 0x20..0x27 inclusive (IN0..IN7 / temp as assigned)
 
 # Helpers
@@ -26,19 +27,30 @@ class ADC128ChannelReading:
         self.volts = volts
 
 class ADC128D818Config:
+    """
+    Configuration object for ADC128D818
+    - start: set START bit
+    - continuous: if False, set low-power conversion (convert all enabled, then shutdown)
+    - disable_mask: bit i disables channel i when set. Must be done in shutdown.
+    - extResistorMultipliers: List of 8 floats, one per channel, to scale readings if using
+        external Vref divider. E.g. for a 10k/30k divider, multiplier is 4.0 (Vout=Vin*10k/40k).
+    """
     start: bool = True
     continuous: bool = True
     disable_mask: int = 0x00
+    mode: int = 0x00 # 0-3, see datasheet
     extResistorMultipliers: List[float] = [1.0] * 8 # if using external Vref divider
 
     def __init__(self,  
                  start: bool = True,
                  continuous: bool = True,
                  disable_mask: int = 0x00,
+                 mode: int = 0x00,
                  extResistorMultipliers: List[float] = [1.0] * 8):
         self.start = start
         self.continuous = continuous
         self.disable_mask = disable_mask
+        self.mode = (mode & 0x03) << 1
         if len(extResistorMultipliers) != 8:
             raise ValueError("extResistorMultipliers must be length 8")
         self.extResistorMultipliers = extResistorMultipliers
@@ -51,41 +63,60 @@ class ADC128D818(I2CDevice):
     TI ADC128D818 – 8ch, 12-bit ΔΣ ADC w/ temp sensor and internal 2.56V Vref.
     """
     def _start(self, enable: bool) -> None:
-        cfg = 0b00000000 
-        self.write_u8(REG_CONFIG, cfg)
-        time.sleep(0.01)  # per datasheet, 10ms after clearing START
         # Bit0 START, Bit1 INT_Enable, Bit3 INT_Clear, Bit7 INITIALIZATION
         cfg = self.read_u8(REG_CONFIG)
-        print(f"_start: cfg before: 0x{cfg:08b}")
         if enable:
             cfg |= 0b00000001
         else:
             cfg &= ~0b00000001
         self.write_u8(REG_CONFIG, cfg)
+        print(f"_start: cfg : 0x{cfg:08b}")
 
     def configure(self,
                   config: ADC128D818Config
                   ) -> None:
         """
-        - start: set START bit
-        - continuous: if False, set low-power conversion (convert all enabled, then shutdown)
-        - disable_mask: bit i disables channel i when set. Must be done in shutdown.
+        - config: configuration object with fields:
+            - start: set START bit
+            - continuous: if False, set low-power conversion (convert all enabled, then shutdown)
+            - disable_mask: bit i disables channel i when set. Must be done in shutdown.
+            - extResistorMultipliers: List of 8 floats, one per channel, to scale readings
         """
         # Enter shutdown to tweak conversion rate & channel disable
         self._start(False)
+        self.config = config
+
+        cfg = 0b10000000 
+        self.write_u8(REG_CONFIG, cfg)
+        time.sleep(0.01)  # per datasheet, 10ms after clearing START
+
+        self.deep_shutdown(True)  # exit deep shutdown if set
+
         # Conversion Rate: bit0=1 continuous, 0 low-power; only valid in shutdown
         self.write_u8(REG_CONV_RATE, 0x01 if config.continuous else 0x00)
+
         # Disable channels (1=disable). Only valid in shutdown.
         self.write_u8(REG_CH_DISABLE, config.disable_mask & 0xFF)
+
+
+        self.write_u8(REG_ADV_CONFIG, self.config.mode)  # advanced config - mode 1
+
         # Start sampling if requested
-        self.config = config
         if config.start:
             self._start(True)
+
+        cfg = self.read_u8(REG_CONFIG)
+        print(f"configure: cfg : 0x{cfg:08b}")
+        print(f"configure: conv_rate : 0x{self.read_u8(REG_CONV_RATE):08b}")
+        print(f"configure: ch_disable : 0x{self.read_u8(REG_CH_DISABLE):08b}")
+        print(f"configure: adv_config : 0x{self.read_u8(REG_ADV_CONFIG):08b}")
 
     def trigger_one_shot(self) -> None:
         """
         When in shutdown (or deep shutdown), writing any value to ONE_SHOT triggers a single conversion.
         """
+        self.deep_shutdown(False)  # exit deep shutdown if set
+        self.write_u8(REG_CONFIG, 0x01)  # ensure START=1
         self.write_u8(REG_ONE_SHOT, 0x01)
 
     def deep_shutdown(self, enable: bool) -> None:
@@ -99,29 +130,44 @@ class ADC128D818(I2CDevice):
     def read_channel_raw(self, index: int) -> int:
         if not (0 <= index <= 7):
             raise ValueError("channel index 0..7")
-        if self.config.continuous is False:
-            self.trigger_one_shot()
-            time.sleep(0.05)  # allow time for conversion;
+        # while self.read_u8(REG_INT_STATUS) & 0x1 == 0:
+        #     time.sleep(0.01)
         return self.read_u16_be(REG_READING_BASE + index)
-
-    def read_channel(self, index: int) -> Tuple[float, int]:
-        if not (0 <= index <= 7):
-            raise ValueError("channel index 0..7")
-        raw = self.read_channel_raw(index)
+    
+    def _convert_raw_to_volts(self, raw: int, index: int) -> float:
         # 12-bit value is left-justified in a 16-bit reg in many TI monitors;
         # Datasheet states 16-bit reg to accommodate 12-bit reading (or 9-bit temp).
         # We assume upper 12 bits; if using full 16, adjust here easily.
         value_12b = (raw >> 4) & 0x0FFF
         volts = value_12b * self.config.extResistorMultipliers[index] * ADC_LSB_V
+        return volts
+
+    def read_channel(self, index: int) -> Tuple[float, int]:
+        if not (0 <= index <= 7):
+            raise ValueError("channel index 0..7")
+        
+        if self.config.continuous is False:
+            self.trigger_one_shot()
+            time.sleep(1)  # allow time for conversion;
+
+        raw = self.read_channel_raw(index)
+        volts = self._convert_raw_to_volts(raw, index)
+        print(f"Reading channel {index}: {volts:9.4f} (raw 0x{raw:04X})")
         return volts, raw
 
     def read_channels(self, active_mask: int = 0xFF) -> Dict[str, Any]:
         readings: Dict[str, Any] = {}
+
+        if self.config.continuous is False:
+            self.trigger_one_shot()
+            time.sleep(1)  # allow time for conversion;
+
         for ch in range(8):
             if (active_mask >> ch) & 0x1:
-                volts, raw = self.read_channel(ch)
-                readings[f"ch_{ch}"] = {"raw":raw, "v": volts}
-                print(f"Reading channel {ch}: {volts:.4f} V (raw 0x{raw:04X})")
+                raw = self.read_channel_raw(ch)
+                volts = self._convert_raw_to_volts(raw, ch)
+                readings[f"ch_{ch}"] = {"raw":raw, "val": volts}
+                print(f"Reading channel {ch}: {volts:9.4f} (raw 0x{raw:04X})")
                 # readings[f"in{ch}_v"] = volts
                 # readings[f"in{ch}_raw"] = raw
         return readings
